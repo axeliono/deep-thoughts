6fc552c914484985ae457158e0cc4cc9daa39c34
axeliono <69375446+axeliono@users.noreply.github.com>
2021-03-18 18:14:20 -0500

    checking for successful branch merge

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+//import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+
+const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//create new ApolloServer and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//integrate ApolloServer with Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    //log where we can go to test GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
